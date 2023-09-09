9cc217223fb35ddc786bd509cc8aab9fd2517acd
Joe-027 <joelabraham098@gmail.com>
2023-09-09 15:47:33 +0530

    Updating the files for Assignment-4.3

---
+++ Week-4/Assingment-4.3/script.js
@@ -0,0 +1,10 @@
+function triangle (n){
+    for (let i = 0; i <= n; i++) {
+      for (let j = 0; j <= i; j++) {
+        process.stdout.write('*');
+      }
+      process.stdout.write("\n");
+    }
+  }
+  
+  triangle(2);
